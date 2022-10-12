7c70d9556fd0ff843aecc1130756b2c3c12a5e14
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-12 21:33:38 +0200

    Exercise concluded.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react"
+import React from "react";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-     <h1>My React App</h1>
+      <h1>My React App - credit card component</h1>
+      <Card
+        data={{
+          name: "Oliveira, Rafael",
+          bank: "Big Bank, Inc",
+          cardNumber1: "1234",
+          cardNumber2: "5678",
+          cardNumber3: "8765",
+          cardNumber4: "4321",
+          expiryDate: "08/19",
+        }}
+      />
     </div>
   );
 }
